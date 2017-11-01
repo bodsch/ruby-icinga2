4bf7fc99b7c749dd4d9db24a8defd08748ff6992
Bodo Schulz <bodo@boone-schulz.de>
2017-11-01 06:51:48 +0100

    fix small problems

--- lib/icinga2/version.rb
+++ lib/icinga2/version.rb
@@ -4,6 +4,6 @@
 module Icinga2
 
   # Current version of gem.
-  VERSION = '1.1.0.pre1' # [Version::MAJOR, Version::MINOR, Version::TINY, Version::PATCH].compact * '.'
+  VERSION = '1.0.0.pre2' # [Version::MAJOR, Version::MINOR, Version::TINY, Version::PATCH].compact * '.'
 
 end
